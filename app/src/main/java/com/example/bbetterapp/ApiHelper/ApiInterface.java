2697b6ead5688231e6dec39e3cc79e4d48b748fc
Jakov Filip Sabolicek <ja.lip132@gmail.com>
2021-05-22 13:35:51 +0200

    api interface setup

--- app/src/main/java/com/example/bbetterapp/ApiHelper/ApiInterface.java
+++ app/src/main/java/com/example/bbetterapp/ApiHelper/ApiInterface.java
@@ -5,25 +5,58 @@ import com.example.bbetterapp.Models.User;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
 
+    /* ----------------------------------------USERS----------------------------------------*/
+    //USERS: GET
     @GET("/users/all")
     Call<List<User>> getUsers();
 
+    //USERS: LOGIN
+    @GET("/users/login/{email}/{password}")
+    Call<String> loginUser(@Path("email") String email, @Path("password") String password);
+
+    //USERS: SAVE
+    @POST("users")
+    Call<User> createUser(@Body User user);
+
+    //USERS: PUT
+    @PUT("users/{id}")
+    Call<User> putUser(@Path("id") String id, @Body User user);
+
+    //USERS: PATCH
+    @PATCH("users/{id}")
+    Call<User> patchUser(@Path("id") String id, @Body User user);
+
+    //USERS: DELETE
+    @DELETE("users/{id}")
+    Call<Void> deleteUser(@Path("id") String id);
+
+    /* ----------------------------------------NOTES----------------------------------------*/
+    //NOTES: GET
     @GET("/notes/all")
     Call<List<User>> getNotes();
 
+    /* ----------------------------------------SESSIONS----------------------------------------*/
+    //SESSIONS: GET
     @GET("/sessions/all")
     Call<List<User>> getSessions();
 
+    /* ----------------------------------------EVENTS----------------------------------------*/
+    //EVENTS: GET
     @GET("/events/all")
     Call<List<User>> getEvents();
 
-    @GET("/users/login/{email}/{password}")
-    Call<String> loginUser(@Path("email") String email, @Path("password") String password);
+
+
 
 }
--- app/src/main/java/com/example/bbetterapp/MainActivity.java
+++ app/src/main/java/com/example/bbetterapp/MainActivity.java
@@ -4,18 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.bbetterapp.ApiHelper.ApiService;
-import com.example.bbetterapp.Models.User;
-
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
     }
 }
